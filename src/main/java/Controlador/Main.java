9b6cedcf7db55779c02547f7a34e329bb1275585
Mapach33 <janomanu23@gmail.com>
2024-11-28 21:53:45 -0500

    delete cositas

--- src/main/java/Controlador/Main.java
+++ src/main/java/Controlador/Main.java
@@ -1,39 +1,20 @@
-
 package Controlador;
+import Vista.Principal;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialLighterIJTheme;
 
-import Modelo.Admin;
-import Modelo.Login;
-import static Modelo.Login.existeUsuarioAdmin;
-import Modelo.User;
-/**
- *
- * @author MATHIAS
- */
+
 public class Main {
 
     public static void main(String[] args) {
-        String filePath = "data/usuarios.txt";  // Archivo donde se guardarán los datos
+        /* Set the Nimbus look and feel */
+        FlatMaterialLighterIJTheme.setup();
 
-        // Registrar admin si no existe ya en el archivo
-        if (!existeUsuarioAdmin(filePath)) {
-            // Crear un usuario admin por defecto (user: "admin", pass: "12345678")
-            Admin adminDefault = new Admin("0001", "Admin", "Principal", "admin@email.com");
-            adminDefault.setUser("admin");   // Usuario predefinido
-            adminDefault.setPass("admin"); // Contraseña predefinida
-            adminDefault.registrarPersona(adminDefault, filePath);
-        }
-
-        // Crear una instancia de la clase Login
-        Login login = new Login(filePath);
-
-        // Iniciar sesión
-        User usuarioLogueado;
-        do {
-            usuarioLogueado = login.iniciarSesion();  // Intentar login
-        } while (usuarioLogueado == null);  // Repetir si el login es incorrecto
-
-        // Mostrar el menú dependiendo del tipo de usuario
-        login.mostrarMenu(usuarioLogueado);
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
     }
 }
 
